0a810fe208a62cb638f5b0f8fc9908aa3bd13a0c
Keith Grochow <keith.grochow@gmail.com>
2016-11-11 06:35:57 +0000

    /halfway through course

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,3 +1,20 @@
-var a = 'variables';
+var express = require('express');
+var app = express();
 
-console.log(a);
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.get('/routing', function(req, res){
+    res.send('hello routing');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port)
+});
